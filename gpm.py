ee816ea53b49bba0ebc6e78a96a904c7b8ad1bf5
Arjun Sohanlal <arjunsohanlal@gmail.com>
2020-06-28 19:47:30 +0530

    Fixed a few login issues
    Changed login to require 2FA and an app password.

--- gpm.py
+++ gpm.py
@@ -8,12 +8,24 @@ trackPrefix = "'title'"
 artistPrefix = "'artist'"
 delim = ","
 
-if not api.oauth_login(api.FROM_MAC_ADDRESS) :
-    api.perform_oauth()
+# Logging out before login
+api.logout()
 
-response = str(api.get_all_songs())
+print("\n\nNote: ==========\nMake sure you've set up 2FA on your Google Account, as well as an App Password for this script. \n================\n")
+
+# Logging in
+android_id = api.FROM_MAC_ADDRESS
+email = input('Email: ');
+app_pwd = input('App password: ')
+api.login(email, app_pwd, android_id, locale='en_US')
+
+# Checking login status
+print('Login status: '+str(api.is_authenticated()))
+
+# Pulling all songs from thumbs up list
+response = str(api.get_top_songs())
+
 index = 0
-
 f = codecs.open("GPMLibraryParsed.txt", mode="w", encoding = "utf-8")
 result = ""
 
@@ -32,6 +44,7 @@ while index < len(response) :
     
     index = index + 1
 
+print("\n\nThumbs up tracks ---\n")
 print(result)
 f.write(result)
 f.close()
